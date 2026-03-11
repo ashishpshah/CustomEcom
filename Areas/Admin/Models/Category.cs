73c8362b84beb93609dc2a8045847b15901ca4df
Gami vishal <vmgami2001@gmail.com>
2026-03-11 18:20:13 +0530

    CategoryModel Change make nullable filed & chnage CategoryController

--- Areas/Admin/Models/Category.cs
+++ Areas/Admin/Models/Category.cs
@@ -3,9 +3,10 @@
 	public class Category : EntitiesBase
 	{
 		public string CategoryName { get; set; }
-		public string ParentCategoryName { get; set; }
-		public string ImagePath { get; set; }
+		public string? ParentCategoryName { get; set; }
+		public string? ImagePath { get; set; }
 		public int? ParentCategoryId { get; set; }
 		public bool ImagePath_Remove { get; set; }
-	}
+        public IFormFile? ImageFile { get; set; }
+    }
 }
