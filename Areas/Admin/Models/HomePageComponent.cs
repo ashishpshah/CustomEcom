2e5501b285b9773a6492cd624ac9a2d39212523f
Vishal Chudasama <vishal@padhyasoft.com>
2026-03-18 16:39:26 +0530

    Home Page Component

---
+++ Areas/Admin/Models/HomePageComponent.cs
@@ -0,0 +1,35 @@
+﻿namespace JewelryStore.Areas.Admin.Models
+{
+	public class HomePageComponent
+	{
+		public long Id { get; set; }
+		public string Key { get; set; }
+		public string Name { get; set; }
+		public string Type { get; set; }
+		public int DisplayOrder { get; set; }
+		public bool IsActive { get; set; }
+		public int ItemId { get; set; }
+		public string RefId { get; set; }
+
+		public List<HomePageComponentItem> Items { get; set; }
+	}
+
+	public class HomePageComponentItem
+	{
+		public long Id { get; set; }
+		public long ComponentId { get; set; }
+
+		public string Title { get; set; }
+		public string Subtitle { get; set; }
+		public string Description { get; set; }
+
+		public string ImagePath { get; set; }
+		public string RedirectUrl { get; set; }
+
+		public string RefId { get; set; }
+		public string RefType { get; set; }
+
+		public int DisplayOrder { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
